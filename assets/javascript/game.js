747bc408174352c96c1cca9529b7a9cbd14d93d9
ebresler <eli@irest.us>
2018-10-04 23:36:55 -0700

    final commit 10/4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,8 +6,8 @@ var lettersUsed = [];
 
 // alphabet array
 var alphabet =
-    ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q"
-    ,"r","s","t","u","v","w","x","y","z"];
+    ["a","b","c","d","e","f","g","h","i","j","k","l","m",
+    "n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 // random computer choice from array
 var randomIndex = Math.floor(Math.random() * alphabet.length);
